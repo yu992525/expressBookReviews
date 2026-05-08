56440b83d41cfc494c7a06174232cf4410a59bfd
yu992525 <celiazhu99@gmail.com>
2026-05-07 20:38:34 -0400

    Refactor book retrieval methods to use Promises

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,82 +1,78 @@
 const express = require('express');
 let books = require("./booksdb.js");
-let isValid = require("./auth_users.js").isValid;
-let users = require("./auth_users.js").users;
 const public_users = express.Router();
-const axios = require('axios'); // Required for Task 10-13 patterns
+const axios = require('axios'); // Required library for Tasks 10-13
 
 // Task 10: Get the book list available in the shop using async-await
 public_users.get('/', async function (req, res) {
-    try {
-        const getBooks = () => {
-            return new Promise((resolve) => {
-                setTimeout(() => resolve(books), 100);
-            });
-        };
-        const list = await getBooks();
-        res.status(200).send(JSON.stringify(list, null, 4));
-    } catch (error) {
-        res.status(500).json({ message: "Error retrieving books" });
-    }
+  try {
+    // Simulating an external API call using a Promise wrapper
+    const getBooks = () => new Promise((resolve) => resolve(books));
+    const list = await getBooks();
+    res.status(200).send(JSON.stringify(list, null, 4));
+  } catch (error) {
+    res.status(500).json({ message: "Error retrieving books" });
+  }
 });
 
-// Task 11: Get book details based on ISBN using Promises
+// Task 11: Get book details based on ISBN using Promise callbacks
 public_users.get('/isbn/:isbn', function (req, res) {
-    const isbn = req.params.isbn;
-    const getBookByISBN = new Promise((resolve, reject) => {
-        setTimeout(() => {
-            if (books[isbn]) {
-                resolve(books[isbn]);
-            } else {
-                reject({ status: 404, message: "Book not found" });
-            }
-        }, 100);
-    });
+  const isbn = req.params.isbn;
+  // Creating a promise to handle the ISBN lookup asynchronously
+  const getBook = new Promise((resolve, reject) => {
+    if (books[isbn]) {
+      resolve(books[isbn]);
+    } else {
+      reject("Book not found");
+    }
+  });
 
-    getBookByISBN
-        .then((book) => res.status(200).send(JSON.stringify(book, null, 4)))
-        .catch((err) => res.status(err.status).json({ message: err.message }));
+  getBook
+    .then((book) => res.status(200).send(JSON.stringify(book, null, 4)))
+    .catch((err) => res.status(404).json({ message: err }));
 });
 
 // Task 12: Get book details based on author using async-await
 public_users.get('/author/:author', async function (req, res) {
-    const author = req.params.author;
-    try {
-        const getBooksByAuthor = await new Promise((resolve) => {
-            const all_isbns = Object.keys(books);
-            const filtered = all_isbns
-                .filter(isbn => books[isbn].author === author)
-                .map(isbn => ({ isbn, ...books[isbn] }));
-            resolve(filtered);
-        });
-        
-        if (getBooksByAuthor.length > 0) {
-            res.status(200).send(JSON.stringify(getBooksByAuthor, null, 4));
-        } else {
-            res.status(404).json({ message: "Author not found" });
-        }
-    } catch (error) {
-        res.status(500).json({ message: "Internal Server Error" });
+  const author = req.params.author;
+  try {
+    // Filtering books by author within an async-await pattern
+    const getByAuthor = await new Promise((resolve) => {
+      const filtered = Object.values(books).filter(b => b.author === author);
+      resolve(filtered);
+    });
+    
+    if (getByAuthor.length > 0) {
+      res.status(200).send(JSON.stringify(getByAuthor, null, 4));
+    } else {
+      res.status(404).json({ message: "Author not found" });
     }
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 
 // Task 13: Get book details based on title using async-await
 public_users.get('/title/:title', async function (req, res) {
-    const title = req.params.title;
-    try {
-        const getBooksByTitle = await new Promise((resolve) => {
-            const all_isbns = Object.keys(books);
-            const filtered = all_isbns
-                .filter(isbn => books[isbn].title === title)
-                .map(isbn => ({ isbn, ...books[isbn] }));
-            resolve(filtered);
-        });
+  const title = req.params.title;
+  try {
+    // Filtering books by title using an asynchronous promise
+    const getByTitle = await new Promise((resolve) => {
+      const filtered = Object.values(books).filter(b => b.title === title);
+      resolve(filtered);
+    });
 
-        if (getBooksByTitle.length > 0) {
-            res.status(200).send(JSON.stringify(getBooksByTitle, null, 4));
-        } else {
-            res.status(404).json({ message: "Title not found" });
-        }
+    if (getByTitle.length > 0) {
+      res.status(200).send(JSON.stringify(getByTitle, null, 4));
+    } else {
+      res.status(404).json({ message: "Title not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+module.exports.general = public_users;
     } catch (error) {
         res.status(500).json({ message: "Internal Server Error" });
     }
